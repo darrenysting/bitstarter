be66c8f85bf7259ee672710e27658a6f448e9930
darrenysting <darren.ys.ting@gmail.com>
2013-07-12 01:39:24 +0000

    Update web.js index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var my_index = fs.readFileSync("index.html");
+   response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
